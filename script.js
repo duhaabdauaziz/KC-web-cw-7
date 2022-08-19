c03e18c41da12f275a18ad5bf6ca35fb05510ce5
duhaabdauaziz <109864552+duhaabdauaziz@users.noreply.github.com>
2022-08-19 14:49:45 +0300

    cw7

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function BMI(weight, height) {
+    let result = weight / (height * height);
+    return result;
+}
 
+console.log(BMI(52, 1,6))
+
+function Status(BMI) {
+    if (bmi < 18.5) {
+        return ("لديك نقص في الوزن")
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return"وزنك صحي";
+    } else {
+        return "لديك زيادة في الوزن";
+    }
+}
+
+function calculate() {
+    let weight = document.logEIementById("weight").value;
+    let height = document.logEIementById("height").value;
+
+    let bmi = BMI(weight,height);
+
+    let desc = Status(bmi);
+
+    let div = document.logEIementById("result")
+
+    div.innerText = bmi + " == " + desc
+}
